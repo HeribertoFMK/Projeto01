df33ff8e9fbfe3005d04fe8d9f45fcaed115d5f7
HeribertoFMK <hfmk2015@gmail.com>
2021-11-17 10:21:45 +0900

    Inicializado e decorardo formulario de contato

---
+++ Site01/Controllers/ContatoController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Site01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site01.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult ReceberContato([FromForm]Contato contato)
+        {
+
+            if (ModelState.IsValid)
+            {
+                string conteudo = string.Format("Nome:{0},E-mail:{1},Assunto:{2} ,Menssagem:{3}", contato.Nome, contato.Email, contato.Assunto, contato.Menssagem);
+
+                return new ContentResult() { Content = conteudo };
+
+            }
+            else
+            {
+                return View("Index");
+            }
+            
+        }
+
+        //Obter Dados Manualmente
+        /*public ActionResult ReceberContato()
+        {
+            //Post - Request.Form
+            //Get - Request.Query ou Request.QueryString
+
+            Contato contato = new Contato();
+
+            contato.Nome = Request.Form["nome"]; 
+            contato.Email = Request.Form["email"];
+            contato.Assunto = Request.Form["assunto"];
+            contato.Mensagem = Request.Form["menssagem"];
+
+            string conteudo = string.Format("Nome:{0},E-mail:{1},Assunto:{2} ,Menssagem:{3}",contato.Nome,contato.Email,contato.Assunto,contato.Mensagem);
+
+            return new ContentResult(){Content = conteudo };
+        }*/
+    }
+}
---
+++ Site01/Models/Contato.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site01.Models
+{
+    public class Contato
+    {
+        [Required(ErrorMessage ="O Campo Nome é Obrigatorio")]
+        [MaxLength(50,ErrorMessage ="O Campo deve conter no maximo 50 caracteres")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O Campo Email é Obrigatorio")]
+        [MaxLength(70,ErrorMessage = "O Campo deve conter no maximo 70 caracteres")]
+        [EmailAddress(ErrorMessage ="Email invalido!")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "O Campo Assunto é Obrigatorio")]
+        [MaxLength(70, ErrorMessage = "O Campo deve conter no maximo 70 caracteres")]
+        public string Assunto { get; set; }
+        [Required(ErrorMessage = "O Campo Mensagem é Obrigatorio")]
+        [MaxLength(2000, ErrorMessage = "O Campo deve conter no maximo 2000 caracteres")]
+        public string Menssagem { get; set; }
+    }
+}
